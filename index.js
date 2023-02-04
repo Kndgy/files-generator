f1fab904d72cfda4390ede75f82d03a8dc68bb25
kaine <ghanirafli8@gmail.com>
2023-02-05 05:05:30 +0700

    single files with iteration in the content

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function createOutputFiles() {
 
     await fsp.mkdir(folderName);
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 100; i++) {
         const fileName = `${folderName}/waves${i + 1}.mcfunction`;
         await fsp.writeFile(fileName, `${folderName}/title @p title waves ${i +1 } started\nsummon bridge:mob_checker\nsummon armor_stand ~ ~ ~`);
         // await fsp.writeFile(fileName, data[i].output);
@@ -31,3 +31,16 @@ async function createOutputFiles() {
 }
 
 createOutputFiles();
+
+async function  createSingleFiles() {
+    let fileContent = "";
+
+    for (let i = 0; i < 3; i++) {
+        fileContent += `test number ${i +1}\n`;
+        // fileContent += `${i + 1}. ${data[i].output}\n`;
+    }
+
+    await fsp.writeFile("Output.txt", fileContent); 
+}
+
+createSingleFiles();
